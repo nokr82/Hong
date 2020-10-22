4736e3599c1dcc9b15b09a00f4fe2df6a6e61085
능이소프트 <nokr82@naver.com>
2020-10-22 15:31:09 +0900

    동우등록

---
+++ kakao/common/dbConn.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hong_Dong_Woo
+ * Date: 2020-10-22
+ * Time: 오후 3:27
+ * db연결
+ */
+$dbHost = "localhost";      // 호스트 주소(localhost, 120.0.0.1)
+$dbName = "nokr82";      // 데이타 베이스(DataBase) 이름
+$dbUser = "nokr82";          // DB 아이디
+$dbPass = "zx1411510!";        // DB 패스워드
+
+// PDO + MariaDB 연결하기
+$pdo = new PDO("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPass);
+$statement = $pdo -> query("SELECT CURDATE() AS date FROM DUAL");
+$row = $statement -> fetch(PDO::FETCH_ASSOC);
+echo htmlentities($row['date']);
+
